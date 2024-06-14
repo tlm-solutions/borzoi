092e3c1b4b2bce46d3293e6504f83a47b4d9d07d
Markus Schmidl <markus.schmidl@mailbox.tu-dresden.de>
2024-06-14 12:54:54 +0200

    database schema for tetra decoder failed slots

--- src/schema.rs
+++ src/schema.rs
@@ -13,3 +13,25 @@ diesel::table! {
         arbitrary -> Nullable<Json>,
     }
 }
+
+diesel::table! {
+    tetra_failed_slots (id) {
+        id -> Int8,
+        station -> Uuid,
+        time -> Timestamptz,
+        burst_type -> Int4,
+        slot_type -> Int4,
+        first_slot_logical_channel -> Int4,
+        first_slot_data -> Bytea,
+        first_slot_crc_ok -> Bool,
+        second_slot_present -> Bool,
+        second_slot_logical_channel -> Nullable<Int4>,
+        second_slot_data -> Nullable<Bytea>,
+        second_slot_crc_ok -> Nullable<Bool>,
+    }
+}
+
+diesel::allow_tables_to_appear_in_same_query!(
+    tetra_data,
+    tetra_failed_slots,
+);
